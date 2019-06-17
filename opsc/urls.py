9f8e73f2c4c3130c42d9d58454f22f757c197fc7
jiapeng.li <jiapeng.li@zhaopin.com.cn>
2019-06-17 18:37:54 +0800

    更换模板、调整模板结构目录

--- opsc/urls.py
+++ opsc/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'opsc'
 urlpatterns = [
     path('', views.index, name='index'),
     path('index.html', views.index, name='index'),
+    path('forms.html', views.forms, name='forms'),
+    path('tables.html', views.tables, name='tables'),
+    path('table2html', views.table2, name='table2'),
+    path('bootstrap-elements.html', views.bootstrap_elements, name='bootstrap_elements'),
+    path('bootstrap-grid.html', views.bootstrap_grid, name='bootstrap_grid'),
     # path('add<str:str_id>', views.add_host, name='add_host'),
     path('query_host/<str:query_str>/', views.query_host, name='query_host'),
 ]
--- opsc/views.py
+++ opsc/views.py
@@ -17,7 +17,72 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
 
-    return render(request, 'index.html', context)
+    return render(request, 'opsc/index.html', context)
+
+
+def bootstrap_elements(request):
+    host_data = ""
+    host_list = host_info.objects.all()
+
+    latest_question_list = host_info.objects.order_by('id')
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return render(request, 'opsc/bootstrap-elements.html', context)
+
+def bootstrap_grid(request):
+    host_data = ""
+    host_list = host_info.objects.all()
+
+    latest_question_list = host_info.objects.order_by('id')
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return render(request, 'opsc/bootstrap-grid.html', context)
+
+def forms(request):
+    host_data = ""
+    host_list = host_info.objects.all()
+
+    latest_question_list = host_info.objects.order_by('id')
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return render(request, 'opsc/forms.html', context)
+
+def tables(request):
+    host_data = ""
+    host_list = host_info.objects.all()
+
+    latest_question_list = host_info.objects.order_by('id')
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return render(request, 'opsc/tables.html', context)
+
+
+def table2(request):
+    host_data = ""
+    host_list = host_info.objects.all()
+
+    latest_question_list = host_info.objects.order_by('id')
+
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return render(request, 'opsc/table2.html', context)
+
+
+
 
 
 def query_host(request, query_str):
@@ -26,7 +91,7 @@ def query_host(request, query_str):
         'host_detail': host_detail,
     }
 
-    return render(request, 'opsc/host_detail.html', context)
+    return render(request, 'opsc/host_detail.html.bak', context)
 
 
 def add_host(request, str_id):
